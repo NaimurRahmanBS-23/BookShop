91cf517bb7b5e1be440d687c89b6d830302bbc22
Md.Naimur Rahman <naimur.rahman@brainstation-23.com>
2023-04-17 10:56:55 +0600

    authorBookListAdded

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index(int ? id)
+        {
+           var name=_db.Books.FirstOrDefault()?.AuthorName;
+            var bookList= _db.Books.Where(x => x.AuthorName == name).ToList();
+         
+            return View(bookList);
+        }
+    }
+}
